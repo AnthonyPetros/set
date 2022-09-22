9d3f2e4bc63a6f23a29ebcd8a8d9c518e016081a
Anthony <petros.24@osu.edu>
2022-09-21 22:29:42 -0400

    Added Stats class for consistent statkeeping

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 NUMBER_OF_ACTIVE_CARDS = 12
 SET_SIZE = 3
 require_relative 'game.rb'
-
+require_relative 'stats.rb'
+Stats.new(1,1,1,1)
 Game.new
 
 
---
+++ stats.rb
@@ -0,0 +1,19 @@
+STATS_COUNT = 4
+#File to keep track of a few stats across playthroughs.
+class Stats
+  attr_accessor :sets_found, :sets_attempted, :resets_used
+  def initialize(sets_found, sets_attempted, resets_used, total_attempts)
+    @sets_found = sets_found
+    @sets_attempted = sets_attempted
+    @resets_used = resets_used
+    @total_attempts = total_attempts
+    if File.exists?("stats.txt")
+      lines = File.readlines("stats.txt")
+      @sets_found += lines[0].to_i
+      @sets_attempted += lines[1].to_i
+      @resets_used += lines[2].to_i
+      @total_attempts += lines[3].to_i
+    end
+    File.write("stats.txt","#{@sets_found}\n#{@sets_attempted}\n#{@resets_used}\n#{@total_attempts}")
+  end
+end
